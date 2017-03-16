6a1ce61629df834d47fad7dcd005bc2d4d81e776
Miftah Afina <miftah.afina@gmail.com>
2017-03-16 11:43:44 +0700

    ex 20 - 23

---
+++ ex23.rb
@@ -0,0 +1,16 @@
+# Reading code on the internet
+
+# 1. Go to bitbucket, github, gitorious search ruby
+# 2. Pick random project and click on it
+# 3. Click on the source, find .rb file
+# 4. Start from top to bottom, read the code, take a note!
+# 5. If found any symbol or strage function interest, write them to research later
+
+# Other resources:
+# sourceforge
+# launchpad
+ 
+# The goals are:
+# 1. Finding ruby source code for things you need
+# 2. Reading through the code and looking for files
+# 3. Trying to understand code you find
